94b2b3b42aa5add63a79a72702210c710eaffadc
anupama <anufit6@gmail.com>
2021-10-17 10:40:29 +0530

    Add model Town Add migration for towns Add Model Temperature Add migration for Temperatures

---
+++ app/Models/Temperature.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Temperature extends Model
+{
+    use HasFactory;
+    protected $fillable = ['town_id','temperature'];
+    
+    public function user(){
+        return $this->belongsTo(Town::class, 'town_id');
+    }
+}
